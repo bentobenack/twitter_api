c09d7d9f88610e0f27dd2aeb1f524280820f9330
Bento Benack <bentobenack@gmail.com>
2022-02-12 01:05:05 -0500

    add HTTPSRedirectMiddleware in app.py file

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, status
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from api.v1.users.routes.user import user as user_router
 from api.v1.tweets.routes.tweet import tweet as tweet_router
@@ -22,9 +23,11 @@ app = FastAPI(
     ]
 )
 
-app.include_router(user_router, prefix="/users")
-app.include_router(tweet_router, prefix="/tweets")
-app.include_router(auth_router, prefix="/auth")
+# app.add_middleware(HTTPSRedirectMiddleware)
+
+app.include_router(user_router, prefix="/api/v1/users")
+app.include_router(tweet_router, prefix="/api/v1/tweets")
+app.include_router(auth_router, prefix="/api/v1/auth")
 
 @app.get(path="/", status_code=status.HTTP_200_OK, tags=["Home"])
 def home():
